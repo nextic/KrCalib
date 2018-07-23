e9c2dddbb379de40a241e10fa53586fe13054072
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-07-23 16:10:14 -0500

    Add type Number and labels function.

--- krcal/core_functions.py
+++ krcal/core_functions.py
@@ -1,16 +1,22 @@
 import numpy as np
 import tables as tb
 import pandas as pd
-import glob
+
+import matplotlib.pyplot as plt
 
 from typing import TypeVar
-
 Number = TypeVar('Number', int, float)
 
+from typing import List, Tuple
+
+from   invisible_cities.icaro.hst_functions  import labels
+
 from   invisible_cities.core.core_functions import loc_elem_1d
+from   invisible_cities.core.core_functions import weighted_mean_and_std
+from   krcal.kr_types import PlotLabels
 
 
-def meam_and_std(x : np.array, range : Tuple[Number])->Tuple[Number]:
+def mean_and_std(x : np.array, range : Tuple[Number])->Tuple[Number]:
     """Computes mean and std for an array within a range"""
     x1 = loc_elem_1d(x, find_nearest(x,range[0]))
     x2 = loc_elem_1d(x, find_nearest(x,range[1]))
@@ -43,3 +49,12 @@ def divide_np_arrays(num : np.array, denom : np.array) -> np.array:
     ratio = np.zeros(len(denom))
     np.divide(num, denom, out=ratio, where=ok)
     return ratio
+
+
+def labels(pl : PlotLabels):
+    """
+    Set x and y labels.
+    """
+    plt.xlabel(pl.x)
+    plt.ylabel(pl.y)
+    plt.title (pl.title)
