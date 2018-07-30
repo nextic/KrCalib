af5ab17f6c6106ebb280f294e1a9d3ccd27a2f59
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-07-30 18:22:00 -0500

    Add a test.

--- krcal/histo_functions_test.py
+++ krcal/histo_functions_test.py
@@ -20,10 +20,11 @@ from invisible_cities.core.testing_utils       import FLOAT_ARRAY
 from invisible_cities.core.testing_utils       import random_length_float_arrays
 
 import matplotlib.pyplot as plt
-
+from . histo_functions              import h1d
+from invisible_cities.icaro. hst_functions import shift_to_bin_centers
 
 def test_matplotlib_histo_equals_numpy_histo():
-    Nevt  = int(1e6)
+    Nevt  = int(1e5)
     mean = 100
     sigma = 10
     x = np.random.normal(mean, sigma, Nevt)
@@ -37,3 +38,16 @@ def test_matplotlib_histo_equals_numpy_histo():
 
     assert_allclose(hist, hist2)
     assert_allclose(bins, bins2)
+
+
+def test_h1d():
+    Nevt  = int(1e5)
+    mean = 200
+    sigma = 10
+    nbins =50
+    x = np.random.normal(mean, sigma, Nevt)
+    r = mean - 5 * sigma, mean + 5 * sigma
+    n, b, p = h1d(x, bins=nbins, range=r)
+    imax = np.argmax(n)
+    assert len(n)   == exactly(nbins)
+    assert b[imax - 2] < mean <  b[imax + 2]
