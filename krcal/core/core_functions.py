ec0f70b27388413c521a4fbbdcc3e8a0c13ae281
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-08-06 17:33:11 -0500

    Add new function.

--- krcal/core/core_functions.py
+++ krcal/core/core_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
-from . kr_types import Number
+
+from   typing      import Tuple, List
+from . kr_types    import Number
 
 def find_nearest(array : np.array, value : Number)->Number:
     """Return the array element nearest to value"""
@@ -14,3 +16,19 @@ def divide_np_arrays(num : np.array, denom : np.array) -> np.array:
     ratio = np.zeros(len(denom))
     np.divide(num, denom, out=ratio, where=ok)
     return ratio
+
+
+def file_numbers_from_file_range(file_range : Tuple[int, int])->List[str]:
+    numbers = range(*file_range)
+    N=[]
+    for number in numbers:
+        if number < 10:
+            N.append(f"000{number}")
+        elif 10 <= number < 100:
+            N.append(f"00{number}")
+        elif 100 <= number < 1000:
+            N.append(f"0{number}")
+        else:
+            N.append(f"{number}")
+
+    return N
