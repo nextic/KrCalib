8ca04c5ceef1c9f7d12a7112d535e8f56ab34c3a
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-08-08 17:25:27 -0500

    Functions added.
    1. selection_in_band() for event filtering.
    2. krg_event() to create an KrGEvent
    3. Modify kr_event() so that a mask with a selection can be passed.

--- krcal/core/analysis_functions.py
+++ krcal/core/analysis_functions.py
@@ -3,13 +3,21 @@ import datetime
 from   pandas.core.frame import DataFrame
 import matplotlib.dates  as md
 import matplotlib.pyplot as plt
-import invisible_cities.core.fit_functions as fitf
-from invisible_cities.icaro. hst_functions import shift_to_bin_centers
-from   invisible_cities.core.core_functions import in_range
+from . histo_functions import labels
+from . kr_types        import PlotLabels
 
-from   typing      import Tuple, List, Iterable
-from . kr_types    import Number, Int, Range
-from . kr_types    import KrBins, KrNBins, KrRanges, KrTimes, KrEvent
+import invisible_cities.core.fit_functions as fitf
+from   invisible_cities.core .stat_functions import poisson_sigma
+from   invisible_cities.icaro. hst_functions import shift_to_bin_centers
+from   invisible_cities.core.core_functions  import in_range
+
+from . fit_lt_functions     import fit_lifetime, fit_lifetime_unbined
+from . fit_functions        import fit_slices_1d_gauss
+from   typing               import Tuple, List, Iterable
+from . kr_types             import Number, Int, Range, Sel
+from . kr_types             import KrBins, KrNBins, KrRanges, KrTimes
+from . kr_types             import KrEvent, KrGEvent
+from . kr_types              import HistoPar2, ProfilePar, FitPar
 
 
 def s12_time_profile(kdst       : KrEvent,
@@ -139,17 +147,64 @@ def kr_ranges_and_bins(dst       : DataFrame,
 
 
 
-def kr_event(dst : DataFrame)->KrEvent:
+def kr_event(dst : DataFrame, sel: bool = False, sel_mask : Sel = None)->KrEvent:
     dst_time = dst.sort_values('event')
-    return KrEvent(X   = dst.X.values,
-                   Y   = dst.Y.values,
-                   Z   = dst.Z.values,
-                   R   = dst.R.values,
-                   Phi = dst.R.values,
-                   T   = dst_time.time.values,
-                   S2e = dst.S2e.values,
-                   S1e = dst.S1e.values,
-                   S2q = dst.S2q.values)
+
+    if sel == False:
+        return KrEvent(X   = dst.X.values,
+                       Y   = dst.Y.values,
+                       Z   = dst.Z.values,
+                       R   = dst.R.values,
+                       Phi = dst.R.values,
+                       T   = dst_time.time.values,
+                       S2e = dst.S2e.values,
+                       S1e = dst.S1e.values,
+                       S2q = dst.S2q.values)
+    else:
+        return KrEvent(X   = dst.X.values[sel_mask],
+                       Y   = dst.Y.values[sel_mask],
+                       Z   = dst.Z.values[sel_mask],
+                       R   = dst.R.values[sel_mask],
+                       Phi = dst.R.values[sel_mask],
+                       T   = dst_time.time.values[sel_mask],
+                       S2e = dst.S2e.values[sel_mask],
+                       S1e = dst.S1e.values[sel_mask],
+                       S2q = dst.S2q.values[sel_mask])
+
+
+def krg_event(dst      : DataFrame,
+              Eg       : np.array,
+              Qg       : np.array,
+              sel      :  bool = False,
+              sel_mask : Sel   = None)->KrGEvent:
+
+    dst_time = dst.sort_values('event')
+
+    if sel:
+        return KrGEvent(X   = dst.X.values[sel_mask],
+                        Y   = dst.Y.values[sel_mask],
+                        Z   = dst.Z.values[sel_mask],
+                        R   = dst.R.values[sel_mask],
+                        Phi = dst.R.values[sel_mask],
+                        T   = dst_time.time.values[sel_mask],
+                        S2e = dst.S2e.values[sel_mask],
+                        S1e = dst.S1e.values[sel_mask],
+                        S2q = dst.S2q.values[sel_mask],
+                        Eg  = Eg[sel_mask],
+                        Qg  = Qg[sel_mask])
+    else:
+        return KrGEvent(X   = dst.X.values,
+                        Y   = dst.Y.values,
+                        Z   = dst.Z.values,
+                        R   = dst.R.values,
+                        Phi = dst.R.values,
+                        T   = dst_time.time.values,
+                        S2e = dst.S2e.values,
+                        S1e = dst.S1e.values,
+                        S2q = dst.S2q.values,
+                        Eg  = Eg,
+                        Qg  = Qg)
+
 
 
 def fiducial_volumes(dst     : DataFrame,
@@ -197,3 +252,100 @@ def select_in_XYRange(kre : KrEvent, xyr : Range)->KrEvent:
                    S1e  = kre.S1e[sel],
                    S2q  = kre.S2q[sel],
                    T    = kre.T[sel])
+
+
+
+def selection_in_band(z         : np.array,
+                      e         : np.array,
+                      range_z   : Range,
+                      range_e   : Range,
+                      nbins_z   : int     = 50,
+                      nbins_e   : int     = 100,
+                      nsigma    : float   = 3.5) ->Tuple[np.array, FitPar, FitPar,
+                                                         HistoPar2, ProfilePar]:
+    """ This returns a selection of the events that are inside the Kr E vz Z
+    returns: np.array(bool)
+    """
+
+    zbins  = np.linspace(*range_z, nbins_z + 1)
+    zerror = np.diff(zbins) * 0.5
+    ebins  = np.linspace(*range_e, nbins_e + 1)
+
+    zc = shift_to_bin_centers(zbins)
+    zerror   = np.diff(zbins) * 0.5
+
+    sel_e = in_range(e, *range_e)
+    mean, sigma, chi2, ok = fit_slices_1d_gauss(z[sel_e], e[sel_e], zbins, ebins, min_entries=5e2)
+    e_mean  = mean.value
+    e_sigma = sigma.value
+    # 1. Profile of mean values of e in bins of z
+    #zc, e_mean, e_sigma = fitf.profileX(z, e, nbins_z, range_z, range_e)
+
+
+    #2. Fit two exponentials to e_mmean +- ns_igma * e_sigma defining a band
+
+    y         = e_mean +  nsigma * e_sigma
+    fph, _    = fit_lifetime_unbined(zc, y, nbins_z)
+    y         = e_mean - nsigma * e_sigma
+    fpl, _    = fit_lifetime_unbined(zc, y, nbins_z)
+
+    # 3. Select events in the range defined by the band
+
+    sel_inband = in_range(e, fpl.f(z), fph.f(z))
+
+    # return data
+    hp = HistoPar2(var = z,
+                   nbins = nbins_z,
+                   range = range_z,
+                   var2 = e,
+                   nbins2 = nbins_e,
+                   range2 = range_e)
+
+    pp = ProfilePar(x = zc, xu = zerror, y = e_mean, yu = e_sigma)
+
+    return sel_inband, fpl, fph, hp, pp
+
+
+def plot_selection_in_band(fpl    : FitPar,
+                           fph    : FitPar,
+                           hp     : HistoPar2,
+                           pp     : ProfilePar,
+                           nsigma : float   = 3.5,
+                           figsize=(10,6)):
+    z       = hp.var
+    e       = hp.var2
+    range_z = hp.range
+    range_e = hp.range2
+    nbins_z = hp.nbins
+    nbins_e = hp.nbins2
+
+    zc     = pp.x
+    emean  = pp.y
+    zerror = pp.xu
+    esigma = pp.yu
+
+    zbins  = np.linspace(*range_z, nbins_z + 1)
+    ebins  = np.linspace(*range_e, nbins_e + 1)
+
+
+    fig = plt.figure(figsize=figsize)
+    pltLabels =PlotLabels(x='Energy-like', y='Events', title='true')
+    ax      = fig.add_subplot(1, 1, 1)
+
+    nevt, *_  = plt.hist2d (z, e, (zbins, ebins))
+
+    plt.errorbar(zc, emean, esigma, zerror,
+                 "kp", label="Kr peak energy $\pm 1 \sigma$")
+
+    plt.plot    (zbins,  fpl.f(zbins),  "m", lw=2, label="$\pm "+str(nsigma)+" \sigma$ region")
+    plt.plot    (zbins,  fph.f(zbins),  "m", lw=2)
+    plt.legend()
+    labels(PlotLabels("Drift time (µs)", "S2 energy (pes)", "Energy vs drift"))
+
+
+def selection_info(sel, comment=''):
+    nsel   = np.sum(sel)
+    effsel = 100.*nsel/(1.*len(sel))
+    s = f"Total number of selected candidates {comment}: {nsel} ({effsel:.1f} %)"
+    print(s)
+    return s
