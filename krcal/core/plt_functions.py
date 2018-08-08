90842e9423d8c485e5f6b88d64061ee006018319
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-08-08 17:23:06 -0500

    Add plot functions.

--- krcal/core/plt_functions.py
+++ krcal/core/plt_functions.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from . histo_functions import labels
+from . histo_functions import h1, plot_histo
 from . kr_types        import PlotLabels
 # import matplotlib.dates  as md
 # from   invisible_cities.icaro.mpl_functions import set_plot_labels
@@ -119,30 +120,27 @@ def plot_s1e_vs_z_r_regions(kdsts, krBins, figsize=(14,10)):
     plt.tight_layout()
 
 
-def plot_s2q_vs_z_r_regions(kdsts, krBins, figsize=(14,10)):
+def plot_energy_vs_t(kce, krBins, krRanges, figsize=(14,10)):
 
-    full, fid, core, hcore = kdsts
 
     fig = plt.figure(figsize=figsize)
     fig.add_subplot(2, 2, 1)
-    nevt, *_  = plt.hist2d(full.Z, full.S2q, (krBins.Z, krBins.S2q))
-    plt.colorbar().set_label("Number of events")
-    labels(PlotLabels("Z (mm)", "S2q (pes)", f" full "))
+    (_) = h1(kce.T, bins = krBins.T, range =krRanges.T)
 
     fig.add_subplot(2, 2, 2)
-    nevt, *_  = plt.hist2d(fid.Z, fid.S2q, (krBins.Z, krBins.S2q))
+    nevt, *_  = plt.hist2d(kce.T, kce.E, (krBins.T, krBins.S2e))
     plt.colorbar().set_label("Number of events")
-    labels(PlotLabels("Z (mm)", "S2q (pes)", f" fid "))
+    labels(PlotLabels("T", "E (pes)", f" E (corrected) vs T"))
 
     fig.add_subplot(2, 2, 3)
-    nevt, *_  = plt.hist2d(core.Z, core.S2q, (krBins.Z, krBins.S2q))
+    nevt, *_  = plt.hist2d(kce.T, kce.Q, (krBins.T, krBins.S2q))
     plt.colorbar().set_label("Number of events")
-    labels(PlotLabels("Z (mm)", "S2q (pes)", f" core "))
+    labels(PlotLabels("T", "Q (pes)", f" Q (corrected)  vs T"))
 
     fig.add_subplot(2, 2, 4)
-    nevt, *_  = plt.hist2d(hcore.Z, hcore.S2q, (krBins.Z, krBins.S2q))
+    nevt, *_  = plt.hist2d(kce.T, kce.S1e, (krBins.T, krBins.S1e))
     plt.colorbar().set_label("Number of events")
-    labels(PlotLabels("Z (mm)", "S2q (pes)", f" hard core Z"))
+    labels(PlotLabels("T", "S2q (pes)", f" S1 vs T"))
     plt.tight_layout()
 
 
@@ -224,135 +222,44 @@ def plot_energy_distributions(dst, zbins, s1bins, s2bins, qbins, figsize=(14,10)
         plt.colorbar().set_label("Number of events")
         labels(PlotLabels("S2 (pes)", "Q (pes)", f"Q vs S2"))
 
-#     fig.add_subplot(2, 2, 2)
-#     nevt, *_  = plt.hist2d(kdst.fid.Z, kdst.fid.E, (krBins.Z, krBins.E))
-#     plt.colorbar().set_label("Number of events")
-#     labels("Z (mm)", "E (pes)", f" fid ")
-#
-#     fig.add_subplot(2, 2, 3)
-#     nevt, *_  = plt.hist2d(kdst.core.Z, kdst.core.E, (krBins.Z, krBins.E))
-#     plt.colorbar().set_label("Number of events")
-#     labels("Z (mm)", "E (pes)", f" core ")
-#
-#     fig.add_subplot(2, 2, 4)
-#     nevt, *_  = plt.hist2d(kdst.hcore.Z, kdst.hcore.E, (krBins.Z, krBins.E))
-#     plt.colorbar().set_label("Number of events")
-#     labels("Z (mm)", "E (pes)", f" hard core Z")
-#     plt.tight_layout()
-#
-#
-# def plot_s1_vs_z(kdst, krBins, figsize=(14,10)):
-#     fig = plt.figure(figsize=figsize)
-#     fig.add_subplot(2, 2, 1)
-#     nevt, *_  = plt.hist2d(kdst.full.Z, kdst.full.S1, (krBins.Z, krBins.S1))
-#     plt.colorbar().set_label("Number of events")
-#     labels("Z (mm)", "S1 (pes)", f"full S1 vs Z")
-#
-#     fig.add_subplot(2, 2, 2)
-#     nevt, *_  = plt.hist2d(kdst.fid.Z, kdst.fid.S1, (krBins.Z, krBins.S1))
-#     plt.colorbar().set_label("Number of events")
-#     labels("Z (mm)", "S1 (pes)", f"fid S1 vs Z")
-#
-#     fig.add_subplot(2, 2, 3)
-#     nevt, *_  = plt.hist2d(kdst.core.Z, kdst.core.S1, (krBins.Z, krBins.S1))
-#     plt.colorbar().set_label("core Number of events")
-#     labels("Z (mm)", "S1 (pes)", f"S1 vs Z")
-#
-#     fig.add_subplot(2, 2, 4)
-#     nevt, *_  = plt.hist2d(kdst.hcore.Z, kdst.hcore.S1, (krBins.Z, krBins.S1))
-#     plt.colorbar().set_label("hard core Number of events")
-#     labels("Z (mm)", "S1 (pes)", f"S1 vs Z")
-#     plt.tight_layout()
-#
-# def plot_s2_vs_s1(kdst, krBins, figsize=(14,10)):
-#     fig = plt.figure(figsize=figsize)
-#     fig.add_subplot(2, 2, 1)
-#     nevt, *_  = plt.hist2d(kdst.full.S1, kdst.full.E, (krBins.S1, krBins.E))
-#     plt.colorbar().set_label("Number of events")
-#     labels("S1 (pes)", "S2 (pes)", f"full S2 vs S1")
-#
-#     fig.add_subplot(2, 2, 2)
-#     nevt, *_  = plt.hist2d(kdst.fid.S1, kdst.fid.E, (krBins.S1, krBins.E))
-#     plt.colorbar().set_label("Number of events")
-#     labels("S1 (pes)", "S2 (pes)", f"fid S2 vs S1")
-#     fig.add_subplot(2, 2, 3)
-#
-#     nevt, *_  = plt.hist2d(kdst.core.S1, kdst.core.E, (krBins.S1, krBins.E))
-#     plt.colorbar().set_label("core Number of events")
-#     labels("S1 (pes)", "S2 (pes)", f"core S2 vs S1")
-#     fig.add_subplot(2, 2, 4)
-#
-#     nevt, *_  = plt.hist2d(kdst.hcore.S1, kdst.hcore.E, (krBins.S1, krBins.E))
-#     plt.colorbar().set_label("hard core Number of events")
-#     labels("S1 (pes)", "S2 (pes)", f"hard core S2 vs S1")
-#     plt.tight_layout()
-#
-#
-# def plot_lifetime_T(kfs, timeStamps, ltlim=(2000, 3000),  figsize=(12,6)):
-#     ez0s = [kf.par[0] for kf in kfs]
-#     lts = [np.abs(kf.par[1]) for kf in kfs]
-#     u_ez0s = [kf.err[0] for kf in kfs]
-#     u_lts = [kf.err[1] for kf in kfs]
-#     plt.figure(figsize=figsize)
-#     ax=plt.gca()
-#     xfmt = md.DateFormatter('%d-%m %H:%M')
-#     ax.xaxis.set_major_formatter(xfmt)
-#     plt.errorbar(timeStamps, lts, u_lts, fmt="kp", ms=7, lw=3)
-#     plt.xlabel('date')
-#     plt.ylabel('Lifetime (mus)')
-#     plt.ylim(ltlim)
-#     plt.xticks( rotation=25 )
-#
-# def display_lifetime_maps(Escale : Measurement,
-#                           ELT: Measurement,
-#                           kltl : KrLTLimits,
-#                           XYcenters : np.array,
-#                           cmap = "jet",
-#                           mask = None):
-#
-#     """Display lifetime maps: the mask allow to specify channels
-#     to be masked out (usually bad channels)
-#     """
-#
-#     #fig = plt.figure(figsize=figsize)
-#     #fig.add_subplot(2, 2, 1)
-#     plt.subplot(2, 2, 1)
-#     *_, cb = display_matrix(XYcenters, XYcenters, Escale.value, mask,
-#                             vmin = kltl.Es.min,
-#                             vmax = kltl.Es.max,
-#                             cmap = cmap,
-#                             new_figure = False)
-#     cb.set_label("Energy scale at z=0 (pes)")
-#     labels("X (mm)", "Y (mm)", "Energy scale")
-#
-#     #fig.add_subplot(2, 2, 2)
-#     plt.subplot(2, 2, 2)
-#     *_, cb = display_matrix(XYcenters, XYcenters, Escale.uncertainty, mask,
-#                         vmin = kltl.Eu.min,
-#                         vmax = kltl.Eu.max,
-#                         cmap = cmap,
-#                         new_figure = False)
-#     cb.set_label("Relative energy scale uncertainty (%)")
-#     labels("X (mm)", "Y (mm)", "Relative energy scale uncertainty")
-#
-#     #fig.add_subplot(2, 2, 3)
-#     plt.subplot(2, 2, 3)
-#     *_, cb = display_matrix(XYcenters, XYcenters, ELT.value, mask,
-#                         vmin = kltl.LT.min,
-#                         vmax = kltl.LT.max,
-#                         cmap = cmap,
-#                         new_figure = False)
-#     cb.set_label("Lifetime (µs)")
-#     labels("X (mm)", "Y (mm)", "Lifetime")
-#
-#     #fig.add_subplot(2, 2, 4)
-#     plt.subplot(2, 2, 4)
-#     *_, cb = display_matrix(XYcenters, XYcenters, ELT.uncertainty, mask,
-#                         vmin = kltl.LTu.min,
-#                         vmax = kltl.LTu.max,
-#                         cmap = cmap,
-#                         new_figure = False)
-#     cb.set_label("Relative lifetime uncertainty (%)")
-#     labels("X (mm)", "Y (mm)", "Relative lifetime uncertainty")
-#
-#     plt.tight_layout()
+
+
+def plot_s2e_vs_z_r_regions(kdsts, krBins, figsize=(14,10)):
+
+    full, fid, core, hcore = kdsts
+
+    fig = plt.figure(figsize=figsize)
+    fig.add_subplot(2, 2, 1)
+    nevt, *_  = plt.hist2d(full.Z, full.S2e, (krBins.Z, krBins.S2e))
+    plt.colorbar().set_label("Number of events")
+    labels(PlotLabels("Z (mm)", "S2e (pes)", f" full "))
+
+    fig.add_subplot(2, 2, 2)
+    nevt, *_  = plt.hist2d(fid.Z, fid.S2e, (krBins.Z, krBins.S2e))
+    plt.colorbar().set_label("Number of events")
+    labels(PlotLabels("Z (mm)", "S2e (pes)", f" fid "))
+
+    fig.add_subplot(2, 2, 3)
+    nevt, *_  = plt.hist2d(core.Z, core.S2e, (krBins.Z, krBins.S2e))
+    plt.colorbar().set_label("Number of events")
+    labels(PlotLabels("Z (mm)", "S2e (pes)", f" core "))
+
+    fig.add_subplot(2, 2, 4)
+    nevt, *_  = plt.hist2d(hcore.Z, hcore.S2e, (krBins.Z, krBins.S2e))
+    plt.colorbar().set_label("Number of events")
+    labels(PlotLabels("Z (mm)", "S2e (pes)", f" hard core Z"))
+    plt.tight_layout()
+
+def plot_energy_vs_z_histo_and_profile(e, zc, emean_z, esigma_z,
+                                       bins_e = 50, range_e = (2e+3,14e+3),
+                                       figsize=(10,6)):
+    fig = plt.figure(figsize=figsize)
+    pltLabels =PlotLabels(x='Energy-like', y='Events', title='true')
+    ax      = fig.add_subplot(1, 2, 1)
+    (_)     = h1(e, bins=50, range=(7e+3,11e+3))
+    plot_histo(pltLabels, ax)
+
+    ax      = fig.add_subplot(1, 2, 2)
+    plt.errorbar(zc, emean_z, esigma_z, np.diff(zc)[0]/2, fmt="kp", ms=7, lw=3)
+
+    plt.tight_layout()
