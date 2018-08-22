cf789027e9f1ff6b09f25b1c3aeafb3df10b90e8
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-08-21 22:29:37 -0600

    Adding many functions.

--- krcal/core/map_functions.py
+++ krcal/core/map_functions.py
@@ -8,13 +8,21 @@ from matplotlib.axes         import Axes
 import numpy as np
 import matplotlib
 
-from .kr_types import  KrSector, KrEvent
-from . kr_types import FitType, MapType
-from . kr_types import ASectorMap, TSectorMap, KrWedge
-from typing    import  List, Tuple, Dict, Sequence
+from   invisible_cities.evm.ic_containers  import Measurement
+
+from . stat_functions import  mean_and_std
+
+from . kr_types        import PlotLabels
+from .kr_types         import KrSector, KrEvent
+from . kr_types        import FitType, MapType
+from . kr_types        import FitParTS
+from . kr_types        import ASectorMap, SectorMapTS
+from typing            import List, Tuple, Dict, Sequence, Iterable
 
 
-def rphi_sector_map(nSectors=10, rmax=200)->Tuple[Dict[int, Tuple[float, float]],
+def rphi_sector_map(nSectors : int   =10,
+                    rmax     : float =200,
+                    sphi     : float =45)->Tuple[Dict[int, Tuple[float, float]],
                                      Dict[int, List[Tuple[float]]]]:
     """
     Default map:
@@ -39,7 +47,7 @@ def rphi_sector_map(nSectors=10, rmax=200)->Tuple[Dict[int, Tuple[float, float]]
         R[ns] = (dr * ns, dr* (ns+1))
 
     for ns in range(3, nSectors):
-        PHI[ns] = [(i, i+45) for i in range(0, 360, 45)]
+        PHI[ns] = [(i, i+sphi) for i in range(0, 360, sphi)]
 
     return R, PHI
 
@@ -79,7 +87,6 @@ def define_rphi_sectors(R       : Dict[int,  Tuple[float, float]],
     return RPS
 
 
-
 def wedge_from_sector(s     : KrSector,
                       rmax  : float =200,
                       scale : float =0.1) ->Wedge:
@@ -93,7 +100,8 @@ def set_map_sequential_colors(wedges : Wedge,
 
     return [i + cr[sector] for i in range(len(wedges)) ]
 
-def add_wedge_patches_to_axis(W       :  Dict[int, List[KrWedge]],
+
+def add_wedge_patches_to_axis(W       :  Dict[int, List[KrSector]],
                               ax      :  Axes,
                               cmap    :  Colormap,
                               alpha   :  float,
@@ -103,7 +111,7 @@ def add_wedge_patches_to_axis(W       :  Dict[int, List[KrWedge]],
                               clims   :  Tuple[float, float])->PatchCollection:
 
     for sector, krws in W.items():
-        wedges = [wedge_from_sector(krw, rmax=200, scale=0.5) for krw in krws]
+        wedges = [wedge_from_sector(krw, rmax=rmax, scale=scale) for krw in krws]
         colors = set_map_sequential_colors(wedges, sector, cr)
         p = PatchCollection(wedges, cmap=cmap, alpha=alpha)
         p.set_array(np.array(colors))
@@ -111,7 +119,8 @@ def add_wedge_patches_to_axis(W       :  Dict[int, List[KrWedge]],
         p.set_clim(clims)
     return p
 
-def draw_wedges(W       :  Dict[int, List[KrWedge]],
+
+def draw_wedges(W       :  Dict[int, List[KrSector]],
                 cmap    :  Colormap                = matplotlib.cm.viridis,
                 alpha   :  float                   = 0.4,  # level of transparency
                 rmax    :  float                   = 200,  # the largest radius
@@ -129,118 +138,173 @@ def draw_wedges(W       :  Dict[int, List[KrWedge]],
     plt.show()
 
 
-# def map_minmax(LTM, wmap, verbose):
-#
-#     LT = []
-#     for ltl in LTM.values():
-#         if wmap == MapType.LT or wmap == MapType.E0 or wmap == MapType.chi2:
-#             for m in ltl:
-#                 LT.append(m.value)
-#         else:
-#             for m in ltl:
-#                 LT.append(m.uncertainty)
-#
-#     ltmin = min(LT)
-#     ltmax = max(LT)
-#
-#     if verbose:
-#         print(f'LT min = {ltmin}, LT max = {ltmax}')
-#     return ltmin, ltmax
-#
-# def select_map(wmap):
-#
-#     if wmap == MapType.LT or wmap == MapType.LTu:
-#         LTM = aSM.lt
-#     elif wmap == MapType.E0 or wmap == MapType.E0u:
-#         LTM = aSM.e0
-#     else:
-#         LTM = aSM.chi2
-#
-# def set_wedges(W, wmap, ax, ltmin, ltmax,
-#                rmax=200, scale=0.5, cmap=matplotlib.cm.jet, alpha=0.4):
-#
-#     for wk in W.keys():
-#         wedges = [wedge_from_sector(s, rmax=rmax, scale=scale) for s in W[wk]]
-#         if wmap == MapType.LT or wmap == MapType.E0 or wmap == MapType.chi2:
-#             colors = [(LTM[wk][i]).value for i in range(len(wedges)) ]
-#         else:
-#             colors = [(LTM[wk][i]).uncertainty for i in range(len(wedges)) ]
-#         #print(colors)
-#         p = PatchCollection(wedges, cmap=cmap, alpha=alpha)
-#         p.set_array(np.array(colors))
-#         ax.add_collection(p)
-#         p.set_clim([ltmin, ltmax])
-#
-# def draw_map(W       : Dict[int, List[Tuple[float,float,float,float]]],
-#              aSM     : ASectorMap,
-#              wmap    : MapType = MapType.LT,
-#              verbose : bool = True,
-#              figsize=(10,8)):
-#
-#     fig = plt.figure(figsize=figsize) # give plots a rectangular frame
-#     ax = fig.add_subplot(111)
-#
-#     LTM = select_map(wmap)
-#
-#     ltmin, ltmax = map_minmax(LTM, wmap, verbose)
-#
-#     for wk in W.keys():
-#         wedges = [wedge_from_sector(s, rmax=200, scale=0.5) for s in W[wk]]
-#         if wmap == MapType.LT or wmap == MapType.E0 or wmap == MapType.chi2:
-#             colors = [(LTM[wk][i]).value for i in range(len(wedges)) ]
-#         else:
-#             colors = [(LTM[wk][i]).uncertainty for i in range(len(wedges)) ]
-#         #print(colors)
-#         p = PatchCollection(wedges, cmap=matplotlib.cm.jet, alpha=0.4)
-#         p.set_array(np.array(colors))
-#         ax.add_collection(p)
-#         p.set_clim([ltmin, ltmax])
-#     fig.colorbar(p, ax=ax)
-#
-#     plt.show()
-#
-#
-# def draw_map_time_slice(W       : Dict[int, List[Tuple[float,float,float,float]]],
-#                         aSM     : TSectorMap,
-#                         tslice  : int,
-#                         wmap    : MapType = MapType.LT,
-#                         verbose : bool = True,
-#                         figsize=(10,8)):
-#
-#     fig = plt.figure(figsize=figsize) # give plots a rectangular frame
-#     ax = fig.add_subplot(111)
-#
-#
-#     XTM = select_map(wmap)
-#     if wmap == MapType.LT or wmap == MapType.LTu:
-#         XTM = aSM.lt
-#     elif wmap == MapType.E0 or wmap == MapType.E0u:
-#         XTM = aSM.e0
-#     else:
-#         XTM = aSM.chi2
-#
-#     LTM = {}
-#
-#     for key in XTM.keys():
-#         lts  = XTM[key]
-#         NLT = [lt[tslice] for lt in lts]
-#         LTM[key] = NLT
-#
-#
-#
-#     ltmin, ltmax = map_minmax(LTM, wmap, verbose)
-#
-#     for wk in W.keys():
-#         wedges = [wedge_from_sector(s, rmax=200, scale=0.5) for s in W[wk]]
-#         if wmap == MapType.LT or wmap == MapType.E0 or wmap == MapType.chi2:
-#             colors = [(LTM[wk][i]).value for i in range(len(wedges)) ]
-#         else:
-#             colors = [(LTM[wk][i]).uncertainty for i in range(len(wedges)) ]
-#         #print(colors)
-#         p = PatchCollection(wedges, cmap=matplotlib.cm.jet, alpha=0.4)
-#         p.set_array(np.array(colors))
-#         ax.add_collection(p)
-#         p.set_clim([ltmin, ltmax])
-#     fig.colorbar(p, ax=ax)
-#
-#     plt.show()
+def tsmap_from_fmap(fMap    : Dict[int, List[FitParTS]],
+                    verbose : bool = False) ->SectorMapTS:
+
+    tmChi2  = {}
+    tmE0    = {}
+    tmLT    = {}
+    tmE0u   = {}
+    tmLTu   = {}
+
+    for sector, fps in fMap.items():
+
+        if verbose:
+            print(f' filling maps for sector {sector}')
+            print(f' number of wedges in sector {len(fps)}')
+
+
+        tCHI2 = [fp.c2 for fp in fps]
+        tE0   = [fp.e0 for fp in fps]
+        tLT   = [fp.lt for fp in fps]
+        tE0u  = [fp.e0u for fp in fps]
+        tLTu  = [fp.ltu for fp in fps]
+
+        tmChi2[sector]  = tCHI2
+        tmE0  [sector]  = tE0
+        tmLT  [sector]  = tLT
+        tmE0u [sector]  = tE0u
+        tmLTu [sector]  = tLTu
+
+    return SectorMapTS(chi2  = tmChi2,
+                       e0    = tmE0,
+                       lt    = tmLT,
+                       e0u   = tmE0u,
+                       ltu   = tmLTu)
+
+
+def amap_from_tsmap(tsMap      : SectorMapTS,
+                    ts         : int  = 0,       # if negative take the average
+                    range_e    : Tuple[float, float] = (5000, 13000),
+                    range_chi2 : Tuple[float, float] = (0,3),
+                    range_lt   : Tuple[float, float] = (1800, 3000)) ->ASectorMap:
+
+    def fill_map_ts(tsm : Dict[int, List[float]], ts : int):
+        M = {}
+        for sector, w in tsm.items():
+            M[sector] = [v[ts] for v in w]
+
+        return M
+
+    def fill_maps_av(tsm : Dict[int, List[float]], range_v : Tuple[float, float]):
+        M  = {}
+        Mu = {}
+        for sector, w in tsm.items():
+            T = [mean_and_std(v, range_v) for v in w]
+            P = list(zip(*T))
+            #print(P[0])
+            M[sector] = P[0]
+            Mu[sector] = P[1]
+
+        return M, Mu
+
+    if ts >=0:
+        mChi2  = fill_map_ts(tsMap.chi2, ts)
+        mE0    = fill_map_ts(tsMap.e0, ts)
+        mLT    = fill_map_ts(tsMap.lt, ts)
+        mE0u   = fill_map_ts(tsMap.e0u, ts)
+        mLTu   = fill_map_ts(tsMap.ltu, ts)
+    else:
+        mChi2, _   = fill_maps_av(tsMap.chi2, range_chi2)
+        mE0, mE0u  = fill_maps_av(tsMap.e0, range_e)
+        mLT, mLTu  = fill_maps_av(tsMap.lt, range_lt)
+
+    return ASectorMap(chi2   = mChi2,
+                      e0    = mE0,
+                      lt    = mLT,
+                      e0u   = mE0u,
+                      ltu   = mLTu)
+
+
+def relative_errors(am : ASectorMap)->ASectorMap:
+    mC2  = {}
+    mE0  = {}
+    mLT  = {}
+    mE0u = {}
+    mLTu = {}
+
+
+    for i, L in am.chi2.items():
+        mC2[i] = L
+    for i, L in am.e0.items():
+        mE0[i] = L
+    for i, L in am.lt.items():
+        mLT[i] = L
+    for i, L in am.e0u.items():
+        Le0 = am.e0[i]
+        Le0u =am.e0u[i]
+        mE0u[i] = [100* y/x for x,y in zip(Le0, Le0u)]
+    for i, L in am.ltu.items():
+        Llt = am.lt[i]
+        Lltu =am.ltu[i]
+        mLTu[i] = [100* y/x if x > 0 else -1 for x,y in zip(Llt, Lltu) ]
+
+    return ASectorMap(chi2  = mC2,
+                      e0    = mE0,
+                      lt    = mLT,
+                      e0u   = mE0u,
+                      ltu   = mLTu)
+
+def add_map_values_to_axis(W       :  Dict[int, List[KrSector]],
+                           M       :  Dict[int, List[float]],
+                           ax      :  Axes,
+                           cmap    :  Colormap,
+                           alpha   :  float,
+                           rmax    :  float,
+                           scale   :  float,
+                           clims   :  Tuple[float, float])->PatchCollection:
+
+    for sector, krws in W.items():
+        wedges = [wedge_from_sector(krw, rmax=rmax, scale=scale) for krw in krws]
+        colors = [M[sector][i] for i in range(len(wedges)) ]
+        #print(colors)
+        p = PatchCollection(wedges, cmap=cmap, alpha=alpha)
+        p.set_array(np.array(colors))
+        ax.add_collection(p)
+        p.set_clim(clims)
+    return p
+
+def draw_maps(W       : Dict[int, List[KrSector]],
+              aMap     : ASectorMap,
+              verbose  : bool                = True,
+              cmap    :  Colormap            = matplotlib.cm.viridis,
+              alpha    : float               = 0.4,  # level of transparency
+              rmax     : float               = 200,  # the largest radius
+              scale    : float               = 0.5,  # needed to fit the map
+              figsize  : Tuple[float, float] =(14,10)):
+
+    def map_minmax(LTM):
+        LT = []
+        for l in LTM.values():
+            for v in l:
+                LT.append(v)
+
+        return min(LT), max(LT)
+
+
+    fig = plt.figure(figsize=figsize) # give plots a rectangular frame
+
+    ax = fig.add_subplot(2,2,1)
+    e0m, e0M = map_minmax(aMap.e0)
+    p = add_map_values_to_axis(W, aMap.e0, ax, cmap, alpha, rmax, scale, clims=(e0m, e0M))
+    fig.colorbar(p, ax=ax)
+    plt.title('e0')
+
+    ax = fig.add_subplot(2,2,2)
+    e0um, e0uM = map_minmax(aMap.e0u)
+    p = add_map_values_to_axis(W, aMap.e0u, ax, cmap, alpha, rmax, scale, clims=(e0um, e0uM))
+    fig.colorbar(p, ax=ax)
+    plt.title('e0u')
+
+    ax = fig.add_subplot(2,2,3)
+    ltm, ltM = map_minmax(aMap.lt)
+    p = add_map_values_to_axis(W, aMap.lt, ax, cmap, alpha, rmax, scale, clims=(ltm, ltM))
+    fig.colorbar(p, ax=ax)
+    plt.title('LT')
+
+    ax = fig.add_subplot(2,2,4)
+    ltum, ltuM = map_minmax(aMap.ltu)
+    p = add_map_values_to_axis(W, aMap.ltu, ax, cmap, alpha, rmax, scale, clims=(ltum, ltuM))
+    fig.colorbar(p, ax=ax)
+    plt.title('LTu')
+    plt.show()
