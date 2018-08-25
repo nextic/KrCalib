d22bdb1d816196b87818d4a5d8ccfb903f55cf7d
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-08-25 17:52:07 -0600

    Add declaration of DataFrame

--- krcal/core/kr_types.py
+++ krcal/core/kr_types.py
@@ -8,6 +8,7 @@ from typing      import TypeVar
 from enum        import Enum
 
 from dataclasses import dataclass
+from pandas import DataFrame
 
 from   invisible_cities.types.ic_types import minmax
 from   invisible_cities.evm  .ic_containers  import Measurement
@@ -251,14 +252,22 @@ class SectorMapTS:  # Map in chamber sector containing time series of pars
     e0u   : Dict[int, List[np.array]]
     ltu   : Dict[int, List[np.array]]
 
+# @dataclass
+# class ASectorMap:  # Map in chamber sector containing average of pars
+#     chi2  : Dict[int, List[float]]
+#     e0    : Dict[int, List[float]]
+#     lt    : Dict[int, List[float]]
+#     e0u   : Dict[int, List[float]]
+#     ltu   : Dict[int, List[float]]
 
 @dataclass
 class ASectorMap:  # Map in chamber sector containing average of pars
-    chi2  : Dict[int, List[float]]
-    e0    : Dict[int, List[float]]
-    lt    : Dict[int, List[float]]
-    e0u   : Dict[int, List[float]]
-    ltu   : Dict[int, List[float]]
+    chi2  : DataFrame
+    e0    : DataFrame
+    lt    : DataFrame
+    e0u   : DataFrame
+    ltu   : DataFrame
+
 #------
 
 
