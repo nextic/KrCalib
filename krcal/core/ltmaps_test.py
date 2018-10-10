b078a7c0c4e716ca1bd7d46845395dd9aebeba32
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-10-10 10:47:07 +0200

    Moved test_filenames_from_list to io_functions_test

--- krcal/core/ltmaps_test.py
+++ krcal/core/ltmaps_test.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 
 from  invisible_cities.io.dst_io  import load_dsts
-
-from  krcal.core.kr_types            import KrFileName
-from  krcal.core.io_functions        import filenames_from_list
 from  krcal.core.core_functions      import time_delta_from_time
 from  krcal.core.analysis_functions  import kr_event
 from  krcal.core.fit_lt_functions    import get_time_series
@@ -40,32 +37,6 @@ def time_series(DST):
 def kBins():
     return np.array([-200., -120.,  -40.,   40.,  120.,  200.])
 
-def test_filenames_from_list(DSTDIR, MAPSDIR, LDSTDIR,
-                             dst_filenames, ldst_filename, map_filename, map_filename_ts,
-                             dst_filenames_path, ldst_filename_path, map_filename_path,
-                             map_filename_ts_path):
-
-    krfn = KrFileName(dst_filenames, ldst_filename, map_filename, map_filename_ts, ' ')
-
-    fn =filenames_from_list(krfn, DSTDIR, LDSTDIR, MAPSDIR)
-    fn.input_file_names   == dst_filenames_path
-    fn.output_file_name   == ldst_filename_path
-    fn.map_file_name      == map_filename_path
-    fn.map_file_name_ts   == map_filename_path
-    fn.emap_file_name     == ' '
-
-
-# def test_load_dst(DSTDIR, MAPSDIR, LDSTDIR,
-#                   dst_filenames, ldst_filename, map_filename, map_filename_ts,
-#                   dst_filenames_path, ldst_filename_path, map_filename_path,
-#                   map_filename_ts_path):
-#
-#     krfn = KrFileName(dst_filenames, ldst_filename, map_filename, map_filename_ts, ' ')
-#     fn =filenames_from_list(krfn, DSTDIR, LDSTDIR, MAPSDIR)
-#     dst           = load_dsts(fn.input_file_names, "DST", "Events")
-#     unique_events = ~dst.event.duplicated()
-#     number_of_evts_full = np.count_nonzero(unique_events)
-#     assert number_of_evts_full == len(dst)
 
 
 def test_get_time_series(time_series, DST):
@@ -110,7 +81,7 @@ def test_select_xy_sectors(DST, kBins):
     l = ((neM[0]/neM[4]).values > 0.8).all()
     r = ((neM[0]/neM[4]).values < 1.1).all()
     assert l & r
-    
+
 
 def test_fit_xy_map(DST, kBins):
     dst, DT, kge = DST
