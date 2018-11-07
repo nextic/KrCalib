fd2eeea2a79634ea48b4577ec6e309290262fa4f
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-11-07 20:02:13 +0100

    Remove reference to local data file.

--- krcal_setup.sh
+++ krcal_setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 export PYTHONPATH=$IC:$PYTHONPATH
 export PYTHONPATH=$KRCALIB:$PYTHONPATH
-export IC_DATA='/Users/jjgomezcadenas/Projects/ICDATA'
+#export IC_DATA='/Users/jjgomezcadenas/Projects/ICDATA'
