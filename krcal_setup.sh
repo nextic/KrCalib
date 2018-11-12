869dad391a3ff68db3c761343c6dcced143c2f04
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-11-12 17:20:18 +0100

    Fix setup.
    Adding name of KrCalib variables and commenting out uneeded defs.

--- krcal_setup.sh
+++ krcal_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
-export PYTHONPATH=$IC:$PYTHONPATH
+#export PYTHONPATH=$IC:$PYTHONPATH
+export KRCALIB=$PWD
 export PYTHONPATH=$KRCALIB:$PYTHONPATH
 #export IC_DATA='/Users/jjgomezcadenas/Projects/ICDATA'
