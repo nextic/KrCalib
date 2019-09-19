5334c2a53ef5dc8e1eb8e6957532bf0b7fc4d952
Alberto Usón Andrés <ausonandres@neutrinos1.local>
2019-09-19 16:40:47 +0200

    Add function to write in file general maps.

--- krcal/core/io_functions.py
+++ krcal/core/io_functions.py
@@ -136,6 +136,20 @@ def write_maps_ts(aMaps : Iterable[ASectorMap], ts: np.array, filename : str):
         asm.ltu.to_hdf(filename,  key =f'ltu_{i}',  mode='a')
 
 
+def write_complete_maps(asm      : ASectorMap,
+                        filename : str       )->None:
+
+    asm.chi2.to_hdf(filename, key='chi2', mode='w')
+    asm.e0  .to_hdf(filename, key='e0'  , mode='a')
+    asm.e0u .to_hdf(filename, key='e0u' , mode='a')
+    asm.lt  .to_hdf(filename, key='lt'  , mode='a')
+    asm.ltu .to_hdf(filename, key='ltu' , mode='a')
+    if hasattr(asm, 'mapinfo'):
+        asm.mapinfo.to_hdf(filename, key='mapinfo'       , mode='a')
+    if hasattr(asm, 't_evol'):
+        asm.t_evol .to_hdf(filename, key='time_evolution', mode='a')
+
+
 def read_maps_ts(filename : str)->Tuple[Series, Dict[int, ASectorMap]]:
 
     tsMaps = {}
