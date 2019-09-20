bce797d57d0563dd6b501b43529aaf251d170db0
Alberto Usón Andrés <ausonandres@neutrinos1.local>
2019-09-20 15:21:22 +0200

    Cosmetics.

--- krcal/map_builder/map_builder_functions.py
+++ krcal/map_builder/map_builder_functions.py
@@ -1,24 +1,18 @@
-from typing import Tuple
-from typing import Optional
+from typing      import Tuple
+from dataclasses import dataclass
 import pandas as pd
 import numpy  as np
 import glob
 
 from krcal.core.kr_types  import ASectorMap, type_of_signal
-from krcal.core.io_functions                  import read_maps #
-from krcal.core.map_functions                 import amap_max #
-from krcal.core.correction_functions          import e0_xy_correction #
-from krcal.core.selection_functions           import plot_selection_in_band
-from dataclasses import dataclass
+from krcal.core.selection_functions           import selection_in_band
+
+from krcal.core       .io_functions       import write_complete_maps
 from krcal.core       .histo_functions    import compute_and_save_hist_as_pd
 from krcal.core       .histo_functions    import compute_similar_histo
 from krcal.core       .histo_functions    import normalize_histo_and_poisson_error
 from krcal.core       .kr_parevol_functions import get_number_of_time_bins
 from krcal.map_builder.checking_functions import check_if_values_in_interval
-
-from krcal.map_builder.checking_functions import AbortingMapCreation
-from krcal.core       .io_functions       import write_complete_maps
-
 
 from invisible_cities.core.core_functions  import in_range
 from invisible_cities.io  .dst_io          import load_dsts
@@ -26,7 +20,21 @@ from invisible_cities.reco.corrections_new import read_maps
 from invisible_cities.reco.corrections_new import maps_coefficient_getter
 from invisible_cities.reco.corrections_new import correct_geometry_
 from invisible_cities.reco.corrections_new import norm_strategy
+from invisible_cities.reco.corrections_new import get_normalization_factor
 
+
+
+def e0_xy_correction(map        : ASectorMap                         ,
+                     norm_strat : norm_strategy   = norm_strategy.max):
+    """
+    Temporal function to perfrom IC geometric corrections only
+    """
+    normalization   = get_normalization_factor(map        , norm_strat)
+    get_xy_corr_fun = maps_coefficient_getter (map.mapinfo, map.e0)
+    def geo_correction_factor(x : np.array,
+                              y : np.array) -> np.array:
+        return correct_geometry_(get_xy_corr_fun(x,y))* normalization
+    return geo_correction_factor
 
 @dataclass
 class reference_histograms:
@@ -134,18 +142,21 @@ def selection_nS_mask_and_checking(dst        : pd.DataFrame                ,
     nevts_after  = dst      .event.nunique()
     eff          = nevts_before / nevts_after
 
-    compute_and_save_hist_as_pd(getattr(dst[(dst.s1_peak==0)&(dst.s2_peak==0)],
-                                        column.value),
-                                output_f, column.value,
-                                nbins_hist, range_hist, norm)
+    compute_and_save_hist_as_pd(values     = getattr(dst[(dst.s1_peak==0)&(dst.s2_peak==0)],
+                                                     column.value),
+                                out_file   = output_f,
+                                hist_name  = column.value,
+                                n_bins     = nbins_hist,
+                                range_hist = range_hist,
+                                norm       = norm)
 
     message = "Selection efficiency of "
     message += column.value
     message += "==1 out of range."
-    check_if_values_in_interval(np.array(eff),
-                                interval[0],
-                                interval[1],
-                                message)
+    check_if_values_in_interval(values          = np.array(eff),
+                                low_lim         = interval[0]  ,
+                                up_lim          = interval[1]  ,
+                                raising_message = message      )
     return mask
 
 
@@ -171,23 +182,29 @@ def check_Z_dst(Z_vect   : np.array,
     """
     ref = pd.read_hdf(ref_file, key='Z_hist')
 
-    N_Z, z_Z   = compute_similar_histo(Z_vect, ref)
-    N_Z, err_N = normalize_histo_and_poisson_error(N_Z, z_Z)
+    N_Z, z_Z   = compute_similar_histo(param     = Z_vect,
+                                       reference = ref   )
+    N_Z, err_N = normalize_histo_and_poisson_error(N = N_Z,
+                                                   b = z_Z)
 
     diff     = N_Z - ref.entries
     diff_sig = diff / (err_N+ref.error)
 
     message = "Z distribution very different to reference one."
     message += " May be some error in Z distribution of events."
-    check_if_values_in_interval(diff_sig, -n_sigmas, n_sigmas, message)
+    check_if_values_in_interval(values          = diff_sig ,
+                                low_lim         = -n_sigmas,
+                                up_lim          = n_sigmas ,
+                                raising_message = message  )
     return;
 
 
-def check_rate_and_hist(times    : np.array           ,
-                        output_f : pd.HDFStore        ,
-                        n_dev    : float       = 5    ,
-                        bin_size : int         = 180  ,
-                        normed   : bool        = False)->None:
+def check_rate_and_hist(times      : np.array           ,
+                        output_f   : pd.HDFStore        ,
+                        name_table : str                ,
+                        n_dev      : float       = 5    ,
+                        bin_size   : int         = 180  ,
+                        normed     : bool        = False)->None:
     """
     Raises exception if evolution of rate vs time is
     not flat. It also computes histogram.
@@ -197,6 +214,8 @@ def check_rate_and_hist(times    : np.array           ,
         Time of the events.
     output_f: pd.HDFStore
         File where histogram will be saved.
+    name_table: string
+        Name for the histogram table inside file.
     n_dev: float
         Relative standard deviation to judge if
         distribution is correct.
@@ -215,12 +234,14 @@ def check_rate_and_hist(times    : np.array           ,
     ntimebins  = get_number_of_time_bins(bin_size,
                                          min_time,
                                          max_time)
-    compute_and_save_hist_as_pd(times,
-                                output_f,
-                                table_name = "Rate",
-                                n_bins     = ntimebins,
-                                range_hist = (min_time,
-                                              max_time))
+
+    compute_and_save_hist_as_pd(values    = times      ,
+                                out_file  = output_f   ,
+                                hist_name = name_table ,
+                                n_bins    = ntimebins  ,
+                                range_hist= (min_time  ,
+                                             max_time) ,
+                                norm      = normed     )
 
     n, _ = np.histogram(times, bins=ntimebins,
                         range = (min_time, max_time))
@@ -230,36 +251,24 @@ def check_rate_and_hist(times    : np.array           ,
 
     message = "Relative deviation is greater than the allowed one."
     message += " There must be some issue in rate distribution."
-    check_if_values_in_interval(np.array(rel_dev)        ,
-                                low_lim         = 0      ,
-                                up_lim          = n_dev  ,
-                                raising_message = message)
+    check_if_values_in_interval(values          = np.array(rel_dev),
+                                low_lim         = 0                ,
+                                up_lim          = n_dev            ,
+                                raising_message = message          )
     return;
 
-def e0_xy_correction(map        : ASectorMap                         ,
-                     norm_strat : norm_strategy   = norm_strategy.max):
-     """
-     Temporal function to perfrom IC geometric corrections only
-     """
-    normalization   = get_normalization_factor(map        , norm_strat)
-    get_xy_corr_fun = maps_coefficient_getter (map.mapinfo, map.e0)
-    def geo_correction_factor(x : np.array,
-                              y : np.array) -> np.array:
-        return correct_geometry_(get_xy_corr_fun(x,y))* normalization
-    return geo_correction_factor
 
-
-def eff_sel_band_and_hist(dst       : pd.DataFrame,
-                          boot_map  : str,
-                          norm_strat: norm_strategy             = norm_strategy.max,
-                          range_Z   : Tuple[np.array, np.array] = (10, 550),
-                          range_E   : Tuple[np.array, np.array] = (10.0e+3,14e+3),
-                          nbins_z   : int                       = 50,
-                          nbins_e   : int                       = 50,
-                          nsigma_sel: float                     = 3.5,
-                          eff_min   : float                     = 0.4,
-                          eff_max   : float                     = 0.6
-                          )->np.array:
+def band_selector_and_check(dst       : pd.DataFrame,
+                           boot_map   : ASectorMap,
+                           norm_strat : norm_strategy             = norm_strategy.max,
+                           range_Z    : Tuple[np.array, np.array] = (10, 550),
+                           range_E    : Tuple[np.array, np.array] = (10.0e+3,14e+3),
+                           nbins_z    : int                       = 50,
+                           nbins_e    : int                       = 50,
+                           nsigma_sel : float                     = 3.5,
+                           eff_min   : float                      = 0.4,
+                           eff_max   : float                      = 0.6
+                           )->np.array:
     """
     This function returns a selection of the events that
     are inside the Kr E vz Z band, and checks
@@ -293,7 +302,7 @@ def eff_sel_band_and_hist(dst       : pd.DataFrame,
     ----------
         A  mask corresponding to the selection made.
     """
-    emaps = e0_xy_correcion(boot_map, norm_strat  = norm_strat)
+    emaps = e0_xy_correction(boot_map, norm_strat  = norm_strat)
     E0 = dst.S2e.values * emaps(dst.X.values, dst.Y.values)
 
     sel_krband, _, _, _, _ = selection_in_band(dst.Z, E0,
@@ -305,10 +314,10 @@ def eff_sel_band_and_hist(dst       : pd.DataFrame,
 
     effsel = dst[sel_krband].event.nunique()/dst.event.nunique()
     message = "Band selection efficiency out of range."
-    check_if_values_in_interval(np.array(effsel),
-                                eff_min,
-                                eff_max,
-                                message)
+    check_if_values_in_interval(values          = np.array(effsel),
+                                low_lim         = eff_min         ,
+                                up_lim          = eff_max         ,
+                                raising_message = message         )
 
     return sel_krband
 
@@ -362,22 +371,79 @@ def compute_map(dst : pd.DataFrame, bins : Tuple[int, int], **kwargs) -> ASector
     add_krevol(regularized_maps, dst)
     return regularized_maps
 
-def apply_cuts(dst : pd.DataFrame, allowed_z_sigma : float, **kwargs) -> pd.DataFrame:
-    mask1 = check_ns_cut(dst, 'S1', **kwargs)
-    mask2 = check_ns_cut(dst, 'S2', mask=mask1, **kwargs)
-    check_z_dist(dst, **kwargs)
-    mask3 = z_band_sel(dst, allowed_z_sigma, **kwargs)
+def apply_cuts(dst              : pd.DataFrame       ,
+               S1_signal        : type_of_signal     ,
+               nS1_eff_interval : Tuple[float, float],
+               store_hist_s1    : pd.HDFStore        ,
+               ns1_histo_params : dict               ,
+               S2_signal        : type_of_signal     ,
+               nS2_eff_interval : Tuple[float, float],
+               store_hist_s2    : pd.HDFStore        ,
+               ns2_histo_params : dict               ,
+               ref_Z_histo      : str                ,
+               nsigmas_Zdst     : float              ,
+               bootstrapmap     : ASectorMap         ,
+               band_sel_params  : dict               ,
+               ) -> pd.DataFrame:
+
+    mask1 = selection_nS_mask_and_checking(dst = dst                  ,
+                                           column = S1_signal         ,
+                                           interval = nS1_eff_interval,
+                                           output_f = store_hist_s1   ,
+                                           **ns1_histo_params         )
+    mask2 = selection_nS_mask_and_checking(dst = dst[mask1]           ,
+                                           column = S2_signal         ,
+                                           interval = nS2_eff_interval,
+                                           output_f = store_hist_s2   ,
+                                           **ns2_histo_params         )
+    check_Z_dst(Z_vect   = dst.Z       ,
+                ref_file = ref_Z_histo ,
+                n_sigmas = nsigmas_Zdst)
+    mask3 = band_selector_and_check(dst      = dst[mask1*mask2],
+                                    boot_map = bootstrapmap    ,
+                                    **band_sel_params          )
     all_mask = mask1 * mask2 * mask3
     return dst[all_mask]
 
 def automatic_test(config):
     dst, bootstrapmap, references  = load_data(**locals)
-    rate_check (dst, **locals)
-    dst_passed_cut = apply_cuts(dst, **locals)
-    rate_check(dst_passed_cut, **locals)
+    store_hist = pd.HDFStore(config.file_out_hists, "w", complib=str("zlib"), complevel=4)
 
-    bin_size  = calculate_bins(dst_passed_cut, **locals)
-    final_map = compute_map(dst_passed_cut, bin_size, **locals)
+    check_rate_and_hist(times      = dst.time         ,
+                        output_f   = store_hist       ,
+                        name_table = "rate_before_sel",
+                        n_dev      = config.n_dev_rate,
+                        **config.rate_histo_params    )
+
+    dst_passed_cut = apply_cuts(dst              = dst                    ,
+                                S1_signal        = type_of_signal.nS1     ,
+                                nS1_eff_interval = (config.nS1_eff_min    ,
+                                                    config.nS1_eff_max)   ,
+                                store_hist_s1    = store_hist             ,
+                                ns1_histo_params = config.ns1_histo_params,
+                                S2_signal        = type_of_signal.nS2     ,
+                                nS2_eff_interval = (config.nS2_eff_min    ,
+                                                    config.nS2_eff_max)   ,
+                                store_hist_s2    = store_hist             ,
+                                ns2_histo_params = config.ns2_histo_params,
+                                ref_Z_histo      = config.ref_Z_histo_file,
+                                nsigmas_Zdst     = config.nsigmas_Zdst    ,
+                                bootstrapmap     = bootstrapmap           ,
+                                band_sel_params  = config.band_sel_params )
+
+    check_rate_and_hist(times      = dst_passed_cut.time,
+                        output_f   = store_hist         ,
+                        name_table = "rate_after_sel"   ,
+                        n_dev      = config.n_dev_rate  ,
+                        **config.rate_histo_params      )
+
+    store_hist.close()
+
+    number_of_bins = get_binning_auto(nevt_sel                = dst_passed_cut.event.nunique()  ,
+                                      thr_events_for_map_bins = config.thr_evts_for_sel_map_bins,
+                                      n_bins                  = config.default_n_bins           )
+
+    final_map = compute_map(dst_passed_cut, number_of_bins, **locals)
 
     write_complete_maps(asm      = final_map          ,
                         filename = config.file_out_map)
