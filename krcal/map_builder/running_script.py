aa40eaa5daf0551bda9daaa41eab21c78c6c4d81
Alberto Usón Andrés <ausonandres@neutrinos1.local>
2019-10-04 12:35:37 +0200

    Add hack in running_script.py to avoid logging system.

--- krcal/map_builder/running_script.py
+++ krcal/map_builder/running_script.py
@@ -1,6 +1,12 @@
 from invisible_cities.core.configure import configure
 from krcal.map_builder.map_builder_functions import automatic_test
 import sys
+import logging
+import warnings
+warnings.filterwarnings("ignore")
+logging.disable(logging.DEBUG)
+this_script_logger = logging.getLogger(__name__)
+this_script_logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     config = configure(sys.argv).as_namespace
