58f584bc9293fee65e57282196fe1a722e2c9aa0
Marija Kekic <mmkekic@gmail.com>
2019-10-06 13:46:57 +0200

    Remove test for deleted p_phi functions in map_functions_test

--- krcal/core/map_functions_test.py
+++ krcal/core/map_functions_test.py
@@ -1,13 +1,8 @@
 import os
-import pandas as pd
-import numpy as np
-
-import warnings
 import pytest
 
-from krcal.core.rphi_maps_functions   import rphi_sector_map_def
-from krcal.core.rphi_maps_functions   import define_rphi_sectors
-from krcal.core.io_functions          import read_maps
+
+from invisible_cities.reco.corrections_new          import read_maps
 from krcal.core.map_functions         import add_mapinfo
 
 @pytest.fixture(scope='session')
@@ -18,27 +13,6 @@ def MAPS(MAPSDIR):
     return maps
 
 
-def test_rphi_sector_map_def():
-    rpsmf = rphi_sector_map_def(nSectors =4, rmax =200, sphi =90)
-    R   = rpsmf.r
-    PHI = rpsmf.phi
-
-    for i in range(0,4):
-        assert R[i]   == (0.0 + i * 50, 50.0 + i * 50)
-        assert PHI[i] == [(0, 90), (90, 180), (180, 270), (270, 360)]
-
-
-def test_define_rphi_sectors():
-    rpsmf = rphi_sector_map_def(nSectors =4, rmax =200, sphi =90)
-    rps = define_rphi_sectors(rpsmf)
-
-    for i in range(0,4):
-        krsl = rps[i]
-        for j, krs in enumerate(krsl):
-            assert krs.rmin == i * 50
-            assert krs.rmax == 50 + i * 50
-            assert krs.phimin == j * 90
-            assert krs.phimax == 90 + j * 90
 
 def test_define_mapinfo(MAPS):
     asm = MAPS
