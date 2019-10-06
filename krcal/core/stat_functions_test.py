647f417f576785323d6b13fc5d517c4dd5d53ea9
Marija Kekic <mmkekic@gmail.com>
2019-10-06 13:52:46 +0200

    Fix imports in stat_functions_test

--- krcal/core/stat_functions_test.py
+++ krcal/core/stat_functions_test.py
@@ -4,20 +4,10 @@ Tests for stat_functions
 
 import numpy as np
 
-from pytest        import mark
 from pytest        import approx
-from pytest        import raises
-from flaky         import flaky
-from numpy.testing import assert_array_equal
-from numpy.testing import assert_allclose
 
 from hypothesis            import given, settings
-from hypothesis.strategies import integers
 from hypothesis.strategies import floats
-from invisible_cities.core.testing_utils       import exactly
-from invisible_cities.core.testing_utils       import float_arrays
-from invisible_cities.core.testing_utils       import FLOAT_ARRAY
-from invisible_cities.core.testing_utils       import random_length_float_arrays
 
 from . stat_functions       import gaussian_experiment
 from . stat_functions       import mean_and_std
