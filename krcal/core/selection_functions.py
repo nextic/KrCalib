b984fd6ae6e07061728d7047103a41c3f2078c7d
Marija Kekic <mmkekic@gmail.com>
2019-10-11 14:54:06 +0200

    Remoove plot_selection_in_band.py from selection_functions

--- krcal/core/selection_functions.py
+++ krcal/core/selection_functions.py
@@ -14,29 +14,27 @@ Author: JJGC
 Last revised: Feb, 2019
 
 """
-import numpy as np
+import numpy  as np
 import pandas as pd
-from   pandas.core.frame import DataFrame
 
-import matplotlib.pyplot as plt
-from . histo_functions import labels
-from . kr_types        import PlotLabels
+from typing      import List
+from typing      import Tuple
+from typing      import  Dict
+from pandas      import DataFrame
 
+from   invisible_cities.core .core_functions  import in_range
+from   invisible_cities.icaro. hst_functions  import shift_to_bin_centers
 
-from   invisible_cities.core.core_functions  import in_range
-from   invisible_cities.icaro. hst_functions import shift_to_bin_centers
 from . fit_lt_functions     import fit_lifetime_unbined
 from . fit_functions        import fit_slices_1d_gauss
-
-
-from . kr_types             import Number, Range
-from . kr_types             import HistoPar2, ProfilePar, FitPar
-from . core_functions       import phirad_to_deg
-
-from typing      import List, Tuple, Dict
+from . kr_types             import Number
+from . kr_types             import Range
+from . kr_types             import HistoPar2
+from . kr_types             import ProfilePar
+from . kr_types             import FitPar
 
 import logging
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 
 
 def event_map_df(dstMap : Dict[int, List[DataFrame]])->DataFrame:
@@ -130,51 +128,6 @@ def select_xy_sectors_df(dst        : DataFrame,
     return dstMap
 
 
-# def select_rphi_sectors_df(dst     : DataFrame,
-#                            RPS     : Dict[int, List[KrSector]])-> Dict[int, List[DataFrame]]:
-#     """
-#     Return a dict of KrEvent organized by rphi sector.
-
-#     Parameters
-#     ----------
-#         dst:
-#         The input data frame.
-
-#         RPS:
-#         RPHI selection, a map defining the RPHI wedges.
-
-#     Returns
-#     -------
-#         A DataFrameMap of selections
-#         where for each radial sector (the key in the dict) one has a list
-#         (corresponding to the PHI sectors) of DataFrame (the events selected)
-
-#     """
-
-#     def selection_mask_rphi_sectors(dst     : DataFrame,
-#                                        RPS     : Dict[int, List[KrSector]])->Dict[int, np.array]:
-#         """Returns a dict of selections arranged in a dict of rphi sectors"""
-#         logging.debug(f' --selection_mask_rphi_sectors:')
-#         MSK = {}
-#         for i, rps in RPS.items():
-#             sel_mask = [in_range(dst.R,
-#                                  s.rmin,
-#                                  s.rmax).values & in_range(phirad_to_deg(dst.Phi),
-#                                                            s.phimin,
-#                                                            s.phimax).values for s in rps]
-#             MSK[i] = sel_mask
-
-#         return MSK
-
-
-#     MSK = selection_mask_rphi_sectors(dst, RPS)
-#     RGES = {}
-#     for i, msk in MSK.items():
-#         logging.debug(f' defining kr_event for sector {i}')
-#         RGES[i] = [dst[m] for m in msk]
-#     return RGES
-
-
 def selection_in_band(z         : np.array,
                       e         : np.array,
                       range_z   : Range,
@@ -198,20 +151,14 @@ def selection_in_band(z         : np.array,
     e_sigma = sigma.value
     # 1. Profile of mean values of e in bins of z
     #zc, e_mean, e_sigma = fitf.profileX(z, e, nbins_z, range_z, range_e)
-
-
     #2. Fit two exponentials to e_mmean +- ns_igma * e_sigma defining a band
-
     y         = e_mean +  nsigma * e_sigma
     fph, _, _    = fit_lifetime_unbined(zc, y, nbins_z, range_z)
     y         = e_mean - nsigma * e_sigma
     fpl, _, _ = fit_lifetime_unbined(zc, y, nbins_z, range_z)
-
     # 3. Select events in the range defined by the band
-
     sel_inband = in_range(e, fpl.f(z), fph.f(z))
 
-    # return data
     hp = HistoPar2(var = z,
                    nbins = nbins_z,
                    range = range_z,
@@ -223,47 +170,3 @@ def selection_in_band(z         : np.array,
 
     return sel_inband, fpl, fph, hp, pp
 
-
-def plot_selection_in_band(fpl    : FitPar,
-                           fph    : FitPar,
-                           hp     : HistoPar2,
-                           pp     : ProfilePar,
-                           nsigma : float   = 3.5,
-                           figsize=(10,6)):
-    z       = hp.var
-    e       = hp.var2
-    range_z = hp.range
-    range_e = hp.range2
-    nbins_z = hp.nbins
-    nbins_e = hp.nbins2
-
-    zc     = pp.x
-    emean  = pp.y
-    zerror = pp.xu
-    esigma = pp.yu
-
-    zbins  = np.linspace(*range_z, nbins_z + 1)
-    ebins  = np.linspace(*range_e, nbins_e + 1)
-
-
-    fig = plt.figure(figsize=figsize)
-    pltLabels =PlotLabels(x='Energy-like', y='Events', title='true')
-    ax      = fig.add_subplot(1, 1, 1)
-
-    nevt, *_  = plt.hist2d (z, e, (zbins, ebins))
-
-    plt.errorbar(zc, emean, esigma, zerror,
-                 "kp", label="Kr peak energy $\pm 1 \sigma$")
-
-    plt.plot    (zbins,  fpl.f(zbins),  "m", lw=2, label="$\pm "+str(nsigma)+" \sigma$ region")
-    plt.plot    (zbins,  fph.f(zbins),  "m", lw=2)
-    plt.legend()
-    labels(PlotLabels("Drift time (µs)", "S2 energy (pes)", "Energy vs drift"))
-
-
-def selection_info(sel, comment=''):
-    nsel   = np.sum(sel)
-    effsel = 100.*nsel/(1.*len(sel))
-    s = f"Total number of selected candidates {comment}: {nsel} ({effsel:.1f} %)"
-    print(s)
-    return s
