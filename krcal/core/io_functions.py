9a59f79f4c06a8484e75a60e82ad3555859f0937
Marija Kekic <mmkekic@gmail.com>
2019-10-11 14:52:23 +0200

    rRemove unused write_maps_ts from io_functions

--- krcal/core/io_functions.py
+++ krcal/core/io_functions.py
@@ -4,22 +4,6 @@ import pandas as pd
 from   typing         import Iterable
 from . kr_types       import ASectorMap
 
-
-def write_maps_ts(aMaps : Iterable[ASectorMap], ts: np.array, filename : str):
-
-    assert len(ts) == len(aMaps)
-    tsdf = pd.Series(ts)
-    tsdf.to_hdf(filename, key='ts', mode='w')
-    for i, t in enumerate(ts):
-        asm = aMaps[i]
-
-        asm.chi2.to_hdf(filename, key =f'chi2_{i}', mode='a')
-        asm.e0.to_hdf(filename,   key =f'e0_{i}',   mode='a')
-        asm.e0u.to_hdf(filename,  key =f'e0u_{i}',  mode='a')
-        asm.lt.to_hdf(filename,   key =f'lt_{i}',   mode='a')
-        asm.ltu.to_hdf(filename,  key =f'ltu_{i}',  mode='a')
-
-
 def write_complete_maps(asm      : ASectorMap,
                         filename : str       )->None:
 
