00be24d85ef8babfcb957450690dd090d3efca8d
Marija Kekic <mmkekic@gmail.com>
2019-10-13 20:19:56 +0200

    Move save_hist to io_functions

--- krcal/core/io_functions.py
+++ krcal/core/io_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-
-from   typing         import Iterable
+from   invisible_cities.core.core_functions import shift_to_bin_centers
+from   typing         import Tuple
 from . kr_types       import ASectorMap
 
 def write_complete_maps(asm      : ASectorMap,
@@ -16,3 +16,37 @@ def write_complete_maps(asm      : ASectorMap,
         asm.mapinfo.to_hdf(filename, key='mapinfo'       , mode='a')
     if hasattr(asm, 't_evol'):
         asm.t_evol .to_hdf(filename, key='time_evolution', mode='a')
+
+
+def compute_and_save_hist_as_pd(values     : np.array           ,
+                                out_file   : pd.HDFStore        ,
+                                hist_name  : str                ,
+                                n_bins     : int                ,
+                                range_hist : Tuple[float, float],
+                                norm       : bool               )->None:
+    """
+    Computes 1d-histogram and saves it in a file.
+    The name of the table inside the file must be provided.
+    Parameters
+    ----------
+    values : np.array
+        Array with values to be plotted.
+    out_file: pd.HDFStore
+        File where histogram will be saved.
+    hist_name: string
+        Name of the pd.Dataframe to contain the histogram.
+    n_bins: int
+        Number of bins to make the histogram.
+    range_hist: length-2 tuple (optional)
+        Range of the histogram.
+    norm: bool
+        If True, histogram will be normalized.
+    """
+    n, b = np.histogram(values, bins = n_bins,
+                        range = range_hist,
+                        density = norm)
+    table = pd.DataFrame({'entries': n,
+                          'magnitude': shift_to_bin_centers(b)})
+    out_file.put(hist_name, table, format='table', data_columns=True)
+
+    return
