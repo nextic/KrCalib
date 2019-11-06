9a9d195315b343f847396b9ad0e3af8c02cf7f8d
gonzaponte <gonzaponte@gmail.com>
2019-11-06 14:53:15 +0200

    Add tests for stat_functions

--- krcal/core/stat_functions_test.py
+++ krcal/core/stat_functions_test.py
@@ -5,6 +5,7 @@ Tests for stat_functions
 import numpy as np
 
 from pytest                import approx
+from pytest                import warns
 
 from hypothesis            import given, settings
 from hypothesis.strategies import floats
@@ -76,6 +77,7 @@ def test_mean_and_std_positive(mean, sigma):
               max_value = + 10))
 @settings(max_examples=10)
 
+
 def test_mean_and_std_zero(mean, sigma):
     Nevt  = int(1e5)
     e = gaussian_experiment(nevt=Nevt, mean=mean, std=sigma)
@@ -83,6 +85,7 @@ def test_mean_and_std_zero(mean, sigma):
     mu, std = mean_and_std(e, (mean- 5 * sigma,mean + 5 * sigma))
     assert mu   == approx(mean  , abs=1e-1)
     assert std  == approx(sigma,  abs=1e-1)
+
 
 def test_mean_and_std_nan():
     x = np.array([NN,NN,NN,NN])
@@ -92,4 +95,8 @@ def test_mean_and_std_nan():
     y = np.array([1,2,3,4, NN])
     z = np.array([1,2,3,4])
     np.allclose(mean_and_std(y, (0,10)), mean_and_std(z, (0,10)), rtol=1e-05, atol=1e-05)
-    
+
+
+def test_mean_and_std_warns_empty_slice():
+    with warns(UserWarning, match="warning, empty slice of x = "):
+        mean_and_std(np.arange(5), range_=(5, 10))
