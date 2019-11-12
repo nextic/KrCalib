3e4934bcb385d03466045364d1e76abb67ca8fb2
Brais Palmeiro Pazos <brais.pp@gmail.com>
2019-11-12 11:11:52 +0100

    Add tests for chi2f

--- krcal/core/fit_functions_test.py
+++ krcal/core/fit_functions_test.py
@@ -11,9 +11,44 @@ from   hypothesis.strategies                import floats
 
 from   invisible_cities.evm  .ic_containers import Measurement
 import invisible_cities.database.load_db    as     DB
-
+from invisible_cities.core                  import  fit_functions as fitf
 from . fit_functions                        import sigmoid
 from . fit_functions                        import compute_drift_v
+from . fit_functions                        import chi2f
+
+
+def test_get_chi2f_when_data_equals_error_and_fit_equals_zero():
+    Nevt  = int(1e6)
+    xdata = np.zeros(Nevt) # Dummy value, not needed
+    ydata = np.random.uniform(1, 100, Nevt)
+    errs  = ydata
+    f     = lambda x: np.zeros_like(x)
+    chi2 = chi2f(f, 0, xdata, ydata, errs)
+    assert chi2   == approx(1  , rel=1e-3)
+
+
+
+def test_get_chi2f_when_data_equals_fit():
+    Nevt  = int(1e6)
+    xdata = np.zeros(Nevt) # Dummy value, not needed
+    ydata = np.random.uniform(1, 100, Nevt)
+    f     = lambda x: ydata
+    errs  = ydata**0.5 # Dummy value, not needed
+    chi2  = chi2f(f, 0, ydata, ydata, errs)
+
+    assert chi2   == approx(0., rel=1e-3)
+
+
+def test_chi2f_str_line():
+    def line(x, m, n):
+        return m * x + n
+    y  = np.array([ 9.108e3, 10.34e3,   1.52387e5,   1.6202e5])
+    ey = np.array([ 3.17   , 13.5   ,  70        ,  21       ])
+    x  = np.array([29.7    , 33.8   , 481        , 511       ])
+
+    fit = fitf.fit(line, x, y, seed=(1,1), sigma=ey)
+    f = lambda x: line(x, *fit.values)
+    assert chi2f(f, 2, x, y, ey) == approx(14, rel=1e-02)
 
 
 @given(floats(min_value = -1e4,
