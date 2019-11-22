e6cc4cb5ea5f6c2a89930f4c4e9ef416be516465
Marija Kekic <mmkekic@gmail.com>
2019-11-22 13:28:07 +0100

    Add small number to time_range right limit in get_time_series

--- krcal/core/selection_functions.py
+++ krcal/core/selection_functions.py
@@ -90,7 +90,9 @@ def get_time_series_df(time_bins    : Number,
             List[np.array] : This are the list of masks defining the events in the time series.
 
     """
-    ip = np.linspace(time_range[0], time_range[-1], time_bins+1)
+    #Add small number to right edge to be included with in_range function
+    eps = 1e-12
+    ip = np.linspace(time_range[0], time_range[-1] + eps, time_bins+1)
     masks = np.array([in_range(dst[time_column].values, ip[i], ip[i + 1]) for i in range(len(ip) -1)])
     return shift_to_bin_centers(ip), masks
 
