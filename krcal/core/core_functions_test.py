41409323f202b52fc04e2cdd29ea701e6291b10d
mmkekic <mmkekic@gmail.com>
2020-04-14 15:02:09 +0200

    Copy test for resolution from IC to core_functions_test

--- krcal/core/core_functions_test.py
+++ krcal/core/core_functions_test.py
@@ -4,9 +4,27 @@ Tests for fit_functions
 
 import numpy as np
 from   numpy.testing        import assert_array_equal
-from   .     core_functions import divide_np_arrays
+from   numpy.testing        import assert_equal
+from   numpy.testing        import assert_allclose
+
+from  . core_functions import divide_np_arrays
+from  . core_functions import resolution
 
 def test_divide_np_array():
     x = np.array([10,100,1000,5000])
     y = np.array([2,5,0,100])
     assert_array_equal(divide_np_arrays(x,y), np.array([  5.,  20.,   0.,  50.]))
+
+
+def test_resolution_no_errors():
+    R, Rbb = resolution([None, 1, 1])
+
+    assert_equal(R  .uncertainty, 0)
+    assert_equal(Rbb.uncertainty, 0)
+
+
+def test_resolution_scaling():
+    _, Rbb1 = resolution([None, 1, 1], E_from = 1)
+    _, Rbb2 = resolution([None, 1, 1], E_from = 2)
+
+    assert_allclose(Rbb1.value * 2**0.5, Rbb2.value)
